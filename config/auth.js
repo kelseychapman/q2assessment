e3dec8a9a8b7f488c09e72ecfa598e5796cf301b
Kelsey Chapman <kelseyc.chapman@gmail.com>
2016-10-19 18:48:27 -0600

    please work

---
+++ config/auth.js
@@ -0,0 +1,5 @@
+module.exports = {
+  clientID: '1587972211508982',
+  clientSecret: 'f0ef8a7248880e8e814dd40233049b3c',
+  callbackURL: "https://http://localhost:3000/signup/auth/facebook/callback"
+}
